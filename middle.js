eaa049e5f89bffd8df1cf3cfa30d75f1e689386e
Maciek Lorencowicz <m.lorencowicz@gmail.com>
2020-03-14 09:52:03 +0100

    14

--- middle.js
+++ middle.js
@@ -1,8 +1,26 @@
-function middle(text){
-let pol=text.lentgh/2
-if (pol%2==0) {
-    console.log(text[Math.ceil(text)]+" "+text[Math.floor(pol)])
+/*
+You are going to be given a word. Your job is to return the middle character of the word. 
+If the word's length is odd, return the middle character. 
+If the word's length is even, return the middle 2 characters.
+*/
+
+function middle(text) {
+    if (text.length <= 0) console.log("wpisz jakiś text");
+    else if (text.legth == 1) console.log(text);
+    else {
+
+        let pol = text.length / 2;
+
+        if (text.length % 2 == 0) {
+            let m = Math.ceil(pol);
+            let l = m - 1;
+
+            console.log(text[l] + text[m]);
+        }
+        else {
+
+            console.log(text[Math.floor(pol)]);
+        }
+    }
 }
-else console.log(text[pol]);
-}
-middle("Nabuhodonozor")
+middle("")
